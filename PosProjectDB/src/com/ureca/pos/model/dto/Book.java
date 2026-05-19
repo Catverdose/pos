0897e723ebecb214157b5851869b30c2f5411967
noodle0325 <wkdwldnjs0325@gmail.com>
2026-05-19 13:07:13 +0900

    dto 수정

--- PosProjectDB/src/com/ureca/pos/model/dto/Book.java
+++ PosProjectDB/src/com/ureca/pos/model/dto/Book.java
@@ -1,5 +1,120 @@
 package com.ureca.pos.model.dto;
 
-public class Book {
+import java.io.Serializable;
+import java.util.Objects;
 
+public class Book implements Cloneable, Comparable<Book>, Serializable {
+	private int bookid;          // 상품 고유 식별자 및 바코드 번호
+	private String bookname;     // 편의점 상품명
+	private String publisher;    // 제조사
+	private int price;           // 상품 단가
+	private int stock;           // 실시간 매대 잔여 재고량
+	private String expireDate;   // 💡 변수명과 toString() 명칭을 expireDate로 깔끔하게 통일했습니다!
+
+	public Book() {}
+	
+	public Book(int bookid, String bookname, String publisher, int price, int stock, String expireDate) {
+		this.bookid = bookid;
+		this.bookname = bookname;
+		this.publisher = publisher;
+		this.price = price;
+		this.stock = stock;
+		this.expireDate = expireDate;
+	}
+	
+	@Override
+	public int compareTo(Book o) {
+		// 상품번호(bookid) 기준 오름차순 정렬
+		return Integer.compare(this.bookid, o.bookid); 
+	}
+	
+	/**
+	 * clone() 
+	 * - 객체의 내용이 똑같은 객체를 생성해서 리턴 
+	 * - Object 클래스에서 protected로 선언했기 때문에 함수를 사용하기 위해서는 반드시 Override 해야 한다. 
+	 * - Cloneable 인터페이스를 구현해야 한다. 
+	 * ==> Cloneable 인터페이스를 구현하지 않으면 CloneNotSupportedException이 발생한다. 
+	 */
+	@Override
+	public Object clone() throws CloneNotSupportedException {
+		return super.clone();
+	}
+	
+	/**
+	 * equals(Object obj)
+	 * - 객체의 내용을 비교하는 함수 
+	 * - 편의점 상품 바코드(bookid)가 일치하면 동일 상품으로 판정합니다.
+	 * 💡 (가짜 중복 메서드인 booleanEquals는 깔끔하게 삭제하고 표준 equals로 일원화했습니다.)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (obj instanceof Book) {  // instanceof가 null 검사도 한다. 
+			Book other = (Book) obj;
+			return Objects.equals(bookid, other.bookid);
+		}
+		return false;
+	}
+	
+	// =========================================================================
+	// Getters and Setters
+	// =========================================================================
+	public int getBookid() {
+		return bookid;
+	}
+	public void setBookid(int bookid) {
+		this.bookid = bookid;
+	}
+	public String getBookname() {
+		return bookname;
+	}
+	public void setBookname(String bookname) {
+		this.bookname = bookname;
+	}
+	public String getPublisher() {
+		return publisher;
+	}
+	public void setPublisher(String publisher) {
+		this.publisher = publisher;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public int getStock() {
+		return stock;
+	}
+	public void setStock(int stock) {
+		this.stock = stock;
+	}
+	public String getExpireDate() {
+		return expireDate;
+	}
+	public void setExpireDate(String expireDate) {
+		this.expireDate = expireDate;
+	}
+	
+	/**
+	 * finalize()
+	 * - 객체가 가비지 컬렉터에 의해 메모리에서 해제될 때 호출되는 함수 
+	 */
+	@Override
+	protected void finalize() throws Throwable {
+		System.out.println(this.hashCode() + " Book 객체 수거 완료(finalize)...");
+	}
+	
+	/**
+	 * toString()
+	 * - 객체의 내용을 문자열로 리턴 
+	 */
+	@Override
+	public String toString() {
+		return "Book [bookid=" + bookid + ", bookname=" + bookname + ", publisher=" + publisher + ", price=" + price
+				+ ", stock=" + stock + ", expireDate=" + expireDate + "]";
+	}
+	
+	void notVirtualInvoke() {
+		System.out.println("Employee의 notVirtualInvoke()");
+	}
 }
--- PosProjectDB/src/com/ureca/pos/model/dto/Orders.java
+++ PosProjectDB/src/com/ureca/pos/model/dto/Orders.java
@@ -1,5 +1,119 @@
 package com.ureca.pos.model.dto;
 
-public class Orders {
+import java.io.Serializable;
+import java.util.Objects;
 
+public class Orders implements Cloneable, Comparable<Orders>, Serializable {
+	private int orderid;      // 영수증 고유 트랜잭션 번호 (자동 증가)
+	private int custid;       // 구매한 회원 고유 번호 (Customer FK)
+	private int bookid;       // 구매한 상품 고유 번호 (Book FK)
+	private int saleprice;    // 총 합산 결제 금액 (단가 * 수량)
+	private int quantity;     // [ADD] 고객의 단일 상품 구매 수량
+	private String orderdate; // 주문 일자 (DB의 DATE 타입을 자바에서 String으로 매핑)
+
+	public Orders() {}
+
+	public Orders(int orderid, int custid, int bookid, int saleprice, int quantity, String orderdate) {
+		this.orderid = orderid;
+		this.custid = custid;
+		this.bookid = bookid;
+		this.saleprice = saleprice;
+		this.quantity = quantity;
+		this.orderdate = orderdate;
+	}
+
+	@Override
+	public int compareTo(Orders o) {
+		// 최신 영수증 번호(orderid)가 앞으로 오도록 내림차순 정렬 (포스기 이력 조회 최적화)
+		return Integer.compare(o.orderid, this.orderid);
+	}
+
+	/**
+	 * clone() 
+	 * - 객체의 내용이 똑같은 객체를 생성해서 리턴 
+	 * - Object 클래스에서 protected로 선언했기 때문에 함수를 사용하기 위해서는 반드시 Override 해야 한다. 
+	 * - Cloneable 인터페이스를 구현해야 한다. 
+	 * ==> Cloneable 인터페이스를 구현하지 않으면 CloneNotSupportedException이 발생한다. 
+	 */
+	@Override
+	public Object clone() throws CloneNotSupportedException {
+		return super.clone();
+	}
+
+	/**
+	 * equals(Object obj)
+	 * - 객체의 내용을 비교하는 함수 
+	 * - 영수증 고유 트랜잭션 번호(orderid)가 같으면 동일한 결제 이력으로 판정합니다.
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (obj instanceof Orders) { // instanceof가 null 검사도 한다. 
+			Orders other = (Orders) obj;
+			return Objects.equals(orderid, other.orderid);
+		}
+		return false;
+	}
+
+	// =========================================================================
+	// Getters and Setters
+	// =========================================================================
+	public int getOrderid() {
+		return orderid;
+	}
+	public void setOrderid(int orderid) {
+		this.orderid = orderid;
+	}
+	public int getCustid() {
+		return custid;
+	}
+	public void setCustid(int custid) {
+		this.custid = custid;
+	}
+	public int getBookid() {
+		return bookid;
+	}
+	public void setBookid(int bookid) {
+		this.bookid = bookid;
+	}
+	public int getSaleprice() {
+		return saleprice;
+	}
+	public void setSaleprice(int saleprice) {
+		this.saleprice = saleprice;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	public String getOrderdate() {
+		return orderdate;
+	}
+	public void setOrderdate(String orderdate) {
+		this.orderdate = orderdate;
+	}
+
+	/**
+	 * finalize()
+	 * - 객체가 가비지 컬렉터에 의해 메모리에서 해제될 때 호출되는 함수 
+	 */
+	@Override
+	protected void finalize() throws Throwable {
+		System.out.println(this.hashCode() + " Orders 객체 수거 완료(finalize)...");
+	}
+
+	/**
+	 * toString()
+	 * - 객체의 내용을 문자열로 리턴 
+	 */
+	@Override
+	public String toString() {
+		return "Orders [orderid=" + orderid + ", custid=" + custid + ", bookid=" + bookid + ", saleprice=" + saleprice
+				+ ", quantity=" + quantity + ", orderdate=" + orderdate + "]";
+	}
+
+	void notVirtualInvoke() {
+		System.out.println("Orders의 notVirtualInvoke()");
+	}
 }
